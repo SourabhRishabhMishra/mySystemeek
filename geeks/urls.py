a8546cbd0f01d92c2e84ffeaa7239080299b10db
unknown <sourabhm384@gmail.com>
2020-08-26 06:43:15 +0530

    initial commit

---
+++ geeks/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin 
+
+urlpatterns = [ 
+    #path('admin/', admin.site.urls), 
+    # Enter the app name in following 
+    # syntax for this to work 
+    #path('', include("projectApp.urls")), 
+] 
